18efceb654cb6e500c4f3129e5a4a9956486c569
angelle rina <angellerinac@gmail.com>
2021-08-10 23:38:43 -0400

    header section

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,22 @@ import React from 'react';
 import { FaqsContainer } from '../containers/faqs';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FooterContainer } from '../containers/footer';
+import {HeaderContainer} from '../containers/header'
+import {OptForm} from '../components/'
 
 export default function Home() {
     return (
         <>
+            <HeaderContainer>
+                <OptForm>
+                    <OptForm.Input placeholder="Email Address" />
+                    <OptForm.Button>Get Started</OptForm.Button>
+                    <OptForm.Break />
+                    <OptForm.Text>Ready to watch? Enter your email to create or restart
+                    your membership.</OptForm.Text>
+                </OptForm>
+            </HeaderContainer>
+            
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
