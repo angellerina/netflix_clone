08f952d0432184fd49ef8c659d759a810a89f21f
angelle rina <angellerinac@gmail.com>
2021-08-11 01:27:06 -0400

    sign in section

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
-import Home from './pages/home';
+import { Home, Signin } from './pages';
 
 function App() {
   return (
     <Router>
-        <Switch>
-            <Route path={ROUTES.SIGN_IN}>
-                <p>I will be the sign in page</p>
-            </Route>
-            <Route path={ROUTES.SIGN_UP}>
-                <p>I will be the sign up page</p>
-            </Route>
-            <Route path={ROUTES.BROWSE}>
-                <p>I will be the browse page</p>
-            </Route>
-            <Route path={ROUTES.HOME}>
-                <Home />
-            </Route>
-        </Switch>
-    </Router>
+    <Switch>
+        <Route path={ROUTES.SIGN_IN}>
+            <Signin />
+        </Route>
+        <Route path={ROUTES.SIGN_UP}>
+            <p>I will be the sign up page</p>
+        </Route>
+        <Route path={ROUTES.BROWSE}>
+            <p>I will be the browse page</p>
+        </Route>
+        <Route path={ROUTES.HOME}>
+            <Home />
+        </Route>
+    </Switch>
+</Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@ import ReactDOM from 'react-dom';
 import { GlobalStyles } from './global-styles';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FirebaseContext } from './context/firebase';
+
+const config = {
+  apiKey: "AIzaSyD9BzuFTvo2cZbH0d2c3TRkKwS5M0hUM-w",
+  authDomain: "netflix-clone-ed6c8.firebaseapp.com",
+  projectId: "netflix-clone-ed6c8",
+  storageBucket: "netflix-clone-ed6c8.appspot.com",
+  messagingSenderId: "236889494042",
+  appId: "1:236889494042:web:c90d0f23010cc97ed277a3"
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+        <GlobalStyles />
+        <App />
+    </FirebaseContext.Provider>, 
   </React.StrictMode>,
   document.getElementById('root')
 );
