6a79383af5de65819f265c9d0a52b75d541f0453
angelle rina <angellerinac@gmail.com>
2021-08-11 00:00:46 -0400

    feature section

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,25 @@
 import React from 'react';
+import { Feature, OptForm } from '../components';
 import { FaqsContainer } from '../containers/faqs';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FooterContainer } from '../containers/footer';
-import {HeaderContainer} from '../containers/header'
-import {OptForm} from '../components/'
+import { HeaderContainer } from '../containers/header';
 
 export default function Home() {
     return (
         <>
             <HeaderContainer>
-                <OptForm>
-                    <OptForm.Input placeholder="Email Address" />
-                    <OptForm.Button>Get Started</OptForm.Button>
-                    <OptForm.Break />
-                    <OptForm.Text>Ready to watch? Enter your email to create or restart
-                    your membership.</OptForm.Text>
-                </OptForm>
+            <Feature>
+                    <Feature.Title>Unlimited movies, TV shows and more.</Feature.Title>
+                    <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+                    <OptForm>    
+                        <OptForm.Input placeholder="Email Address" />
+                        <OptForm.Button>Get Started</OptForm.Button>
+                        <OptForm.Break />
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart
+                        your membership.</OptForm.Text>
+                    </OptForm>
+                </Feature>
             </HeaderContainer>
             
             <JumbotronContainer />
