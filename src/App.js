63d061f56e393170b2ce88c55fff92d42b90eccf
angelle rina <angellerinac@gmail.com>
2021-08-11 20:36:13 -0400

    Modified App Routes to be Protected

--- src/App.js
+++ src/App.js
@@ -3,26 +3,31 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { Home, Signin, Signup, Browse } from './pages';
+import { useAuthListener } from './hooks';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+
 
 function App() {
-  return (
-    <Router>
-    <Switch>
-        <Route path={ROUTES.SIGN_IN}>
-            <Signin />
-        </Route>
-        <Route path={ROUTES.SIGN_UP}>
-            <Signup />
-        </Route>
-        <Route path={ROUTES.BROWSE}>
-            <Browse />
-        </Route>
-        <Route path={ROUTES.HOME}>
-            <Home />
-        </Route>
-    </Switch>
-</Router>
-  );
-}
+    const { user } = useAuthListener();
+    return (
+        <Router>
+            <Switch>
+                <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN}>
+                    <Signin />
+                </IsUserRedirect>
+                <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
+                    <Signup />
+                </IsUserRedirect>
+                <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+                    <Browse />
+                </ProtectedRoute>
+                <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+                    <Home />
+                </IsUserRedirect>
+            </Switch>
+        </Router>
+      );
+    }
+    
 
 export default App;
